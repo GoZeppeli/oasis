0e2fcb43ea4c29143d6bb12b1ba71c25f5922033
LedZep <71646253+GoZeppeli@users.noreply.github.com>
2023-11-05 12:44:25 +0100

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React plssssss
+          Learn React noppee
         </a>
       </header>
     </div>
