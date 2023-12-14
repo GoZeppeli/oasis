97cbc4c9ecf0782501326793082b72cefc4f54f5
LedZep <71646253+GoZeppeli@users.noreply.github.com>
2023-12-14 23:03:23 +0100

    nav update

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import {isMobile} from 'react-device-detect';
 export default function Headers(props) {
+
         if(document.querySelector(".here")){
             if(props.bottom && props.right){
                 var here = document.querySelector(".here")
@@ -33,11 +35,11 @@ export default function Headers(props) {
             header.style.height= "120px"
             menu.style.height = "60px"
             menu.style.top = "34px"
-            menuH.style.fontSize = "20px"
-            menuH.style.top = "30px"
-            menuH.style.right = "68px"
+            menuH.style.fontSize = "18px"
+            menuH.style.top = "33px"
+            menuH.style.right = "62px"
             h1.style.fontSize = "60px"
-            h1.style.top = "63px"
+            h1.style.top = "62px"
         } else {
             header.style.height= ""
             menu.style.height = ""
@@ -49,7 +51,21 @@ export default function Headers(props) {
             h1.style.top = ""
         }
     }
-}
+}   
+
+    function hover() {
+        var style = getComputedStyle(document.querySelector(".menuH"))
+        var number = Number(style.right.replace('px', '')) - 3
+        var final = number + "px"
+        document.querySelector(".menuH").style.right = `${final}`
+    }
+
+    function closeBack() {
+        var style = getComputedStyle(document.querySelector(".menuH"))
+        var number = Number(style.right.replace('px', '')) + 3
+        var final = number + "px"
+        document.querySelector(".menuH").style.right = `${final}`
+    }
 
     window.addEventListener("scroll", () => {
         scrollandresize()
@@ -83,7 +99,7 @@ export default function Headers(props) {
             <div className="header-div">
                 <img src={window.location.origin + '/img/protest-crop.png'} alt="header" className="protest"></img>
                 <Link to={"/"}><h1 className="title">Oasis.</h1></Link>
-                <div className="menuLink" onClick={() => openMenu()}>
+                <div className="menuLink" onMouseEnter={() => {hover()}} onMouseLeave={() => closeBack()} onClick={() => openMenu()}>
                     <svg className="menu" id="redMenu" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 316.7 326.17"><defs></defs><path class="cls-1" d="M412.22,216.4c-7.85-26.84-31.41-45.83-61.47-53.39-7.31-41.08-33.47-72.92-68.58-78-30.37-4.39-60,12.36-79.49,41.13-30.52-8.1-60.87-2.65-80.8,17.59-24.59,25-26.39,65.51-8,102.31-19.85,33.53-21.5,71.81-.74,98.24s58.73,34,96.21,22.38c22,32.05,55.86,49.67,88.52,42.08,36.74-8.54,60.81-46.36,62.19-91.5C400.58,294.17,423.08,253.52,412.22,216.4Z" transform="translate(-98.24 -84.33)"/></svg>
                     <h3 className="menuH">menu</h3>
                 </div>
